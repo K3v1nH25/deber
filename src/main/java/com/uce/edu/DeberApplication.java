70cf3fe856fc44a335dd24935c4ed17d548e384e
kevin hurtado <kdhurtado@uce.edu.ec>
2023-11-20 22:55:05 -0500

    parte uno

---
+++ src/main/java/com/uce/edu/DeberApplication.java
@@ -0,0 +1,22 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeberApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Crear un nuevo proyecto");
+		
+	}
+
+}
